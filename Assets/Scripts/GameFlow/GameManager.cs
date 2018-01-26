ca5b72b2a57490cd76b14f4a58197db9d1c8bcb3
Thorsten Schmiedel <schmiedel.thorsten@gmail.com>
2018-01-26 23:29:02 +0100

    first gameflow iteration

--- Assets/Scripts/GameFlow/GameManager.cs
+++ Assets/Scripts/GameFlow/GameManager.cs
@@ -5,73 +5,90 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    #region Private Methods
-    private void Init()
-    {
-        LoadSaveGames();
-    }
-    #endregion
+	#region Private Methods
+	private void Init()
+	{
+		LoadSaveGames();
+	}
+	#endregion
 
-    #region Processing Methods
+	#region Processing Methods
 
-    #region Loading of save games
-    private void LoadSaveGames()
-    {
-        // Get the list of save games from disk
-        List<SaveGame> saveGameListOnDisk = LoadSaveGamesFromDisk();
+	#region Loading of save games
+	private void LoadSaveGames()
+	{
+		// Get the list of save games from disk
+		List<SaveGame> saveGameListOnDisk = LoadSaveGamesFromDisk();
 
-        // Override old list with the new list from disk
-        m_saveGameList = saveGameListOnDisk;
-    }
+		// Override old list with the new list from disk
+		m_saveGameList = saveGameListOnDisk;
+	}
 
-    private List<SaveGame> LoadSaveGamesFromDisk()
-    {
-        string directoryPath = Path.GetDirectoryName(m_saveGamePath);
-        bool directoryExists = Directory.Exists(directoryPath);
+	private List<SaveGame> LoadSaveGamesFromDisk()
+	{
+		string directoryPath = Path.GetDirectoryName(m_saveGamePath);
+		bool directoryExists = Directory.Exists(directoryPath);
 
-        Debug.Assert(directoryExists, "The directory of the save game folder does not exist");
-        if (!directoryExists)
-        {
-            return null;
-        }
+		Debug.Assert(directoryExists, "The directory of the save game folder does not exist");
+		if (!directoryExists)
+		{
+			return null;
+		}
 
-        string[] saveGameFileArray = Directory.GetFiles(directoryPath); // Array containing all save game files
-        List<SaveGame> saveGameList = new List<SaveGame>(saveGameFileArray.Length); //< Create list with a capacity equal to the amount of save game files
+		string[] saveGameFileArray = Directory.GetFiles(directoryPath); // Array containing all save game files
+		List<SaveGame> saveGameList = new List<SaveGame>(saveGameFileArray.Length); //< Create list with a capacity equal to the amount of save game files
 
-        foreach (string fileName in Directory.GetFiles(directoryPath))
-        {
-            string fileContent = File.ReadAllText(fileName);
-            var saveGame = JsonUtility.FromJson<SaveGame>(fileContent);
+		foreach (string fileName in Directory.GetFiles(directoryPath))
+		{
+			string fileContent = File.ReadAllText(fileName);
+			var saveGame = JsonUtility.FromJson<SaveGame>(fileContent);
 
-            // Check if save game was successfully loaded
-            Debug.Assert(saveGame != null, string.Format("Savegame could not be loaded from file {0}", fileName));
-            if (saveGame == null)
-            {
-                continue;
-            }
+			// Check if save game was successfully loaded
+			Debug.Assert(saveGame != null, string.Format("Savegame could not be loaded from file {0}", fileName));
+			if (saveGame == null)
+			{
+				continue;
+			}
 
-            // Add loaded savegame to the save game list
-            m_saveGameList.Add(saveGame);
-        }
+			// Add loaded savegame to the save game list
+			saveGameList.Add(saveGame);
+		}
 
-        // Return the list of save games
-        return m_saveGameList;
-    }
-    #endregion
+		// Return the list of save games
+		return saveGameList;
+	}
+	#endregion
 
-    #region Saving of save games
-    private void SaveCurrentGame()
-    {
+	#region Saving of save games
+	private void SaveCurrentGame()
+	{
+		Debug.Assert(m_activeGameInstance != null, "Tried to save the game, but there is no active game instance");
+		if (m_activeGameInstance == null)
+		{
+			return;
+		}
+		SaveGame saveGame = m_activeGameInstance.CreateSaveGame();
 
-    }
-    #endregion
+		Debug.Assert(saveGame != null, "Tried to save the current game, but the save game returned by the instance was invalid");
 
-    #region Private Fields
-    private GameInstance m_activeGameInstance = null;
+		string jsonContent = JsonUtility.ToJson(saveGame);
 
-    [SerializeField]
-    private string m_saveGamePath = "";
+		// Save the save game to disk
+		FileUtilities.CreateOrOverwriteAllText(m_saveGamePath, jsonContent);
+	}
+	#endregion
+	#endregion
 
-    private List<SaveGame> m_saveGameList = new List<SaveGame>();
-    #endregion
+	#region Helper Methods
+
+	#endregion
+
+	#region Private Fields
+	private GameInstance m_activeGameInstance = null;
+
+	[SerializeField]
+	private string m_saveGamePath = "";
+
+	private List<SaveGame> m_saveGameList = new List<SaveGame>();
+	#endregion
 }
---
+++ Assets/Scripts/Utility/FileUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+public static class FileUtilities
+{
+	public static void CreateOrOverwriteAllText(string filePath, string content)
+	{
+		string directory = Path.GetDirectoryName(filePath);
+
+		if (!Directory.Exists(directory))
+		{
+			Directory.CreateDirectory(directory);
+		}
+
+		File.WriteAllText(filePath, content);
+	}
+}
