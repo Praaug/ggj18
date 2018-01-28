80d7ad1a3e1066d6468f83f3a37c4785e69204e8
Fabian <f1clan@web.de>
2018-01-28 13:05:00 +0100

    Added Sound Manager

---
+++ Assets/Scripts/UI/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public SoundManager instance => new SoundManager();
+
+    public SoundManager()
+    {
+
+    }
+
+    public void Init(GameViewModel gameViewModel)
+    {
+        gameViewModel.OnUpdateMenu += GameViewModel_OnUpdateMenu;
+    }
+
+    private void GameViewModel_OnUpdateMenu(BaseViewModel newModel, BaseViewModel oldModel)
+    {
+        
+    }
+
+    [SerializeField]
+    private AudioSource m_mainMenuSource;
+
+    [SerializeField]
+    private AudioSource m_ingameSource;
+
+    [SerializeField]
+    private float m_fadeTime;
+}
