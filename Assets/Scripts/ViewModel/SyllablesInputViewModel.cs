a9ab293d445fcaad3adb6e5e4699671a14d71310
equusligneus <reher.frederik@gmail.com>
2018-01-28 14:37:48 +0100

    - added sound - added easter egg language - presumably fixed win/loss conditions

--- Assets/Scripts/ViewModel/SyllablesInputViewModel.cs
+++ Assets/Scripts/ViewModel/SyllablesInputViewModel.cs
@@ -13,11 +13,26 @@ public class SyllablesInputViewModel : BaseViewModel
 
     public SyllableViewModel[] DisplayedSyllables => m_DisplayedSyllables;
 
+    public bool CanTransmit
+    {
+        get
+        {
+            return m_CanTransmit;
+        }
+        set
+        {
+            m_CanTransmit = value;
+            OnCanTransmitChanged?.Invoke(value);
+        }
+    }
+
     public event Action OnAcceptCommand;
 
     public event Action OnSwitchTableCommand;
 
     public event Action OnSyllablesChanged;
+
+    public event Action<bool> OnCanTransmitChanged;
 
     public SyllablesInputViewModel(GameViewModel gameViewModel) : base(gameViewModel) { }
 
@@ -97,6 +112,8 @@ public class SyllablesInputViewModel : BaseViewModel
             m_InTableSyllables[i].SetFromSyllable(inSyllables[m_ShuffledInLanguageIndexes[i]]);
             m_OutTableSyllables[i].SetFromSyllable(outSyllables[transmission.Conversion[m_ShuffledInLanguageIndexes[i]]]);
         }
+
+        CanTransmit = false;
     }
 
     public void OnInputCommand(int inputIndex)
@@ -112,6 +129,10 @@ public class SyllablesInputViewModel : BaseViewModel
         m_DisplayedSyllables[m_InputIndex].SetFromSyllable(lang.GetSyllables()[syllableIndex]);
 
         m_InputIndex = (m_InputIndex + 1) % m_Session.SessionParams.SyllableSearchedAmount;
+        if (m_InputIndex == 0)
+        {
+            CanTransmit = true;
+        }
     }
 
     public SyllablesInputViewModel() : base()
@@ -148,4 +169,6 @@ public class SyllablesInputViewModel : BaseViewModel
     private Session m_Session;
 
     private int m_InputIndex;
+
+    private bool m_CanTransmit;
 }
